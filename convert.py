a6413e839c3bd595912f6b60b0755efde45f815a
Mrinalini Nair Ani <91671136+hacksh4w@users.noreply.github.com>
2023-10-07 16:42:58 +0530

    refactor : fix indentation issues

--- convert.py
+++ convert.py
@@ -7,7 +7,9 @@ json_output = '/folder/trajectory.json'
 trajectory_data = []
 
 with open(gmat_op, 'r') as gmat_file:
-    # Skip the header
+    #reset file pointer
+    gmat_file.seek(0) 
+    # Skip the headers
     next(gmat_file)
     
     for line in gmat_file:
@@ -21,19 +23,27 @@ with open(gmat_op, 'r') as gmat_file:
         z_position = float(values[6])
         #print(x_position, y_position,z_position)
 
-        time_step = {
+        '''time_step = {
             "UTCGregorian": date_time.strftime('%Y %b %d %H:%M:%S.%f'),
             "EarthMJ2000Eq": {
                 "X": x_position,
                 "Y": y_position,
                 "Z": z_position
             }
+        }'''
+
+        time_step = {
+                "Date": date_time.strftime('%d %b %Y'),
+                "Time": date_time.strftime('%H:%M:%S.%f')[:-3],  # Remove the last 3 characters (milliseconds)
+                "X": x_position,
+                "Y": y_position,
+                "Z": z_position
         }
 
         trajectory_data.append(time_step)
 
 json_data = {
-    "trajectory": trajectory_data
+    "trajectory" :trajectory_data
 }
 
 with open(json_output, 'w') as json_file:
